ba2c9665a2084b3bdfdb85311aefaa2dd2ea5547
Lucas Faleiro <lucas.faleirosilva@gmail.com>
2025-11-12 16:41:44 -0300

    exercicio 2

--- src/PainelGlicemico.jsx
+++ src/PainelGlicemico.jsx
@@ -1,19 +1,41 @@
-import { use, useState } from "react"
+import {  useReducer, useState } from "react"
+
+const fatorReducer = (state, action) => {
+    const { type } = action;
+    switch (type) {
+        case 'SET_FIC':
+            return {...state, fic: action.payload };
+        case 'SET_ALVO':
+            return {...state, alvo: action.payload };
+        default:
+            return state;
+    }
+}
 
 const PainelGlicemico = () => {
-    const [currGlycemia, setCurrentGlycemia] = useState('120');
-    const [mealCarbs, setMealCarbs] = useState('50');
-    const [fic, setFic] = useState('10');
-    const [targetGlycemia] = useState('100');
+    const [glicemiaAtual, setGlicemiaAtual] = useState('120');
+    const [carbsRefeicao, setCarbsRefeicao] = useState('50');
+    const [controleGlicemico, dispatch] = useReducer(fatorReducer, {
+        fic: 10,
+        alvo: 100
+    });
 
-    console.log(currGlycemia, mealCarbs)
+    // console.log(currGlycemia, mealCarbs)
+    const handleAjusteRapido = () => {
+        dispatch({type: 'SET_FIC', payload: 8});
+        dispatch({type: 'SET_ALVO', payload: 90});
+    }
+
+    console.log(controleGlicemico.fic, controleGlicemico.alvo);
+    
 
     return (
         <div>
             <label htmlFor="curr-glycemia">Glicemia Atual:</label>
-            <input type="number" name="curr-glycemia" id="curr-glycemia" min={0} onChange={(e) => setCurrentGlycemia(e.currentTarget.value)} />
+            <input type="number" name="curr-glycemia" id="curr-glycemia" min={0} onChange={(e) => setGlicemiaAtual(e.currentTarget.value)} />
             <label htmlFor="meal-carbs">Carboidratos da Refeição:</label>
-            <input type="number" name="meal-carbs" id="meal-carbs" min={0} onChange={(e) => setMealCarbs(e.currentTarget.value)} />
+            <input type="number" name="meal-carbs" id="meal-carbs" min={0} onChange={(e) => setCarbsRefeicao(e.currentTarget.value)} />
+            <button onClick={handleAjusteRapido}>Ajuste Rápido</button>
         </div>
     )
 }
